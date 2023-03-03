de86d31af1dbd8ecf05eab143e12b89c24fd7bbd
Dmitry Yurov <yurov37@gmail.com>
2023-03-04 00:05:33 +0200

    add cart to item and remove

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import ProductsScreen from "./screens/ProductsScreen.jsx";
+import CartScreen from "./screens/CartScreen.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/products" element={<ProductsScreen />}></Route>
+            <Route path="/cart" element={<CartScreen />}></Route>
           </Routes>
         </main>
       </Router>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import products from "./slices/products.js";
+import cart from "./slices/cart.js";
 
 const reducer = combineReducers({
   products,
+  cart,
 });
 
 export default configureStore({
